d415dae8559290562a7d8a8d34cd2c6a3a5512cc
tavisca-pkadam <pkadam@tavisca.in>
2019-07-04 11:19:14 +0530

    Solved

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,46 +23,72 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            Console.WriteLine(equation);
+            // Console.WriteLine(equation);
             var op1 = Regex.Split(equation, @"[*,=]");
-            Console.WriteLine("op1 contains ?");
-            Console.WriteLine(op1[0].Contains('?'));
-
-            Console.WriteLine(op1[0]);
-            Console.WriteLine(op1[1]);
-            Console.WriteLine(op1[2]);
+            var re_op = "";
+            // Console.WriteLine(op1[0]);
+            // Console.WriteLine(op1[1]);
+            // Console.WriteLine(op1[2]);
             string ans = "";
             // if op1 and op2 both do not contain ?
-            Console.WriteLine("op1 contains ?");
-            Console.WriteLine(op1[0].Contains('?'));
 
             if(op1[0].Contains("?") )
             {
-                Console.WriteLine("op1  contains ?");
-                ans = (Int32.Parse(op1[2]) / Int32.Parse(op1[0])).ToString();
-                op1[0].Replace( '?' , ans[op1[0].IndexOf("?")] );
+                // Console.WriteLine("op1  contains ?");
+                
+                // System.Console.WriteLine(Int32.Parse(op1[2]) / Int32.Parse(op1[1]));
+                ans = (Double.Parse(op1[2]) / Double.Parse(op1[1])).ToString();
+                // System.Console.WriteLine(op1[0].IndexOf("?"));
+                // System.Console.WriteLine(ans[0]);
 
-                Console.WriteLine(op1[0]);
+                re_op = op1[0].Replace( '?' , ans[op1[0].IndexOf("?")] );
+                
+                if(ans.Equals(re_op)){
+                    var x =  ans[op1[0].IndexOf("?")];
+                    // Console.WriteLine(result);
+                    return Int16.Parse(x.ToString());
+                }else{
+                    return -1;
+                }
             }
             else if( op1[1].Contains("?"))
             {
-                Console.WriteLine("op2  contains ?");
-                ans = (Int32.Parse(op1[2]) / Int32.Parse(op1[1])).ToString();
-                op1[1].Replace( '?' , ans[op1[1].IndexOf("?")] );
+                // Console.WriteLine("op2  contains ?");
+                // System.Console.WriteLine(Int32.Parse(op1[2]) / Int32.Parse(op1[0]));
+                ans = (Double.Parse(op1[2]) / Double.Parse(op1[0])).ToString();
+                // System.Console.WriteLine(op1[1].IndexOf("?"));
+                // System.Console.WriteLine(ans[0]);
 
-                Console.WriteLine(op1[1]);
+                re_op = op1[1].Replace( '?' , ans[op1[1].IndexOf("?")] );
+                
+                if(ans.Equals(re_op)){
+                    var x =  ans[op1[1].IndexOf("?")];
+                    // Console.WriteLine(result);
+                    return Int16.Parse(x.ToString());
+                }else{
+                    return -1;
+                }
             }
             else{
-                Console.WriteLine("op3  contains ?");
+                // Console.WriteLine("op3  contains ?");
+                // System.Console.WriteLine(Int32.Parse(op1[2]) / Int32.Parse(op1[1]));
+                ans = (Double.Parse(op1[0]) * Double.Parse(op1[1])).ToString();
+                // Console.WriteLine(op1[1].IndexOf("?"));
+                // System.Console.WriteLine(ans[0]);
+
+                re_op = op1[2].Replace( '?' , ans[op1[2].IndexOf("?")] );
+                
+                if(ans.Equals(re_op)){
+                    var x =  ans[op1[2].IndexOf("?")];
+                    // Console.WriteLine(result);
+                    return Int16.Parse(x.ToString());
+                }else{
+                    return -1;
+                }
+
+                
             }
 
-
-            // if op1 or op2 contain ?
-
-
-
-            return 0;
-            // Add your code here.
         }
 
        
