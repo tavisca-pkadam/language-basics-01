25637993854698fe140ca08bc1a6ddc5052e0f84
tavisca-pkadam <pkadam@tavisca.in>
2019-07-04 07:45:55 +0530

    Init Solution

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;  
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -22,8 +23,48 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
+            Console.WriteLine(equation);
+            var op1 = Regex.Split(equation, @"[*,=]");
+            Console.WriteLine("op1 contains ?");
+            Console.WriteLine(op1[0].Contains('?'));
+
+            Console.WriteLine(op1[0]);
+            Console.WriteLine(op1[1]);
+            Console.WriteLine(op1[2]);
+            string ans = "";
+            // if op1 and op2 both do not contain ?
+            Console.WriteLine("op1 contains ?");
+            Console.WriteLine(op1[0].Contains('?'));
+
+            if(op1[0].Contains("?") )
+            {
+                Console.WriteLine("op1  contains ?");
+                ans = (Int32.Parse(op1[2]) / Int32.Parse(op1[0])).ToString();
+                op1[0].Replace( '?' , ans[op1[0].IndexOf("?")] );
+
+                Console.WriteLine(op1[0]);
+            }
+            else if( op1[1].Contains("?"))
+            {
+                Console.WriteLine("op2  contains ?");
+                ans = (Int32.Parse(op1[2]) / Int32.Parse(op1[1])).ToString();
+                op1[1].Replace( '?' , ans[op1[1].IndexOf("?")] );
+
+                Console.WriteLine(op1[1]);
+            }
+            else{
+                Console.WriteLine("op3  contains ?");
+            }
+
+
+            // if op1 or op2 contain ?
+
+
+
+            return 0;
             // Add your code here.
-            throw new NotImplementedException();
         }
+
+       
     }
 }
